d67619d718992da0e658f1bcabd1a38b90528bab
DanielScardigno <danielscardigno2@gmail.com>
2025-06-18 14:34:26 +0200

    finished exercise

---
+++ spring-la-mia-pizzeria-crud/src/main/java/org/lessons/java/wdpt6/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -0,0 +1,61 @@
+package org.lessons.java.wdpt6.spring_la_mia_pizzeria_crud.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pizzas")
+public class Pizza {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+    private String descrizione;
+    private String foto;
+    private double prezzo;
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrizione() {
+		return this.descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public String getFoto() {
+		return this.foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public double getPrezzo() {
+		return this.prezzo;
+	}
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+}
